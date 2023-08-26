8cac188cc939eee3f5d6dee7a4f529a0bfcba7f7
ctingelhoff <ctingelhoff@outlook.com>
2023-08-26 16:37:56 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -2,14 +2,15 @@
  * Author: Caden Tingelhoff
  * Date: 08/26/2023
  *
- * A simple program that prints my name.
+ * A simple program that prints my name and major.
 */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int args, char **argv) {
 
-    printf("Caden Tingelhoff\n");
+    printf("My name is Caden Tingelhoff!\n");
+    printf("I am majoring in Computer Science.\n");
 
     return 0;
 }
