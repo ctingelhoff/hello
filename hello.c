20dfb3dcd956bc961c644e41b4a5ab717cdf2aa9
ctingelhoff <ctingelhoff@outlook.com>
2023-08-26 15:47:12 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Caden Tingelhoff
+ * Date: 08/26/2023
+ *
+ * A simple program that prints my name.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int args, char **argv) {
+
+    printf("Caden Tingelhoff\n");
+
+    return 0;
+}
